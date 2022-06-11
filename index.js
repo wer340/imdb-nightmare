37d8575332d8210452ce7b84f52cea7f78219e72
wer340 <hsee1372@gmail.com>
2022-06-11 17:15:44 +0430

    get title most popular movie from imdb

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+const request=require("request-promise");
+const cheerio=require("cheerio");
+
+
 const sampleResult={
-    title:"The Lost City"
+    title:"The Lost City",
     rank:15,
     imdbRating:8.4,
     descriptionThe :"https://www.imdb.com/title/tt13320622/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=ea4e08e1-c8a3-47b5-ac3a-75026647c16e&pf_rd_r=TPQSP59R5B3EESAK4Z04&pf_rd_s=center-1&pf_rd_t=15506&pf_rd_i=moviemeter&ref_=chtmvm_tt_15",
     posterUrl:"https://www.imdb.com/title/tt13320622/mediaviewer/rm3056854785/?ref_=tt_ov_i"
 
-
+};
+async function scarpTitlesRanksAndRaating (){
+const result= await request.get("https://www.imdb.com/chart/moviemeter/?ref_=nv_mv_mpm");
+const $=await cheerio.load(result);
+const movie =$("td.titleColumn > a").map((i,element)=>{
+return($(element).text())
+}).get();
+console.log(movie)
 }
+
+scarpTitlesRanksAndRaating();
