3629573bccc0e578fc973e3137cd3253ba9cb255
wer340 <hsee1372@gmail.com>
2022-06-11 16:47:59 +0430

    initial package  part till 3 course

---
+++ index.js
@@ -0,0 +1,9 @@
+const sampleResult={
+    title:"The Lost City"
+    rank:15,
+    imdbRating:8.4,
+    descriptionThe :"https://www.imdb.com/title/tt13320622/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=ea4e08e1-c8a3-47b5-ac3a-75026647c16e&pf_rd_r=TPQSP59R5B3EESAK4Z04&pf_rd_s=center-1&pf_rd_t=15506&pf_rd_i=moviemeter&ref_=chtmvm_tt_15",
+    posterUrl:"https://www.imdb.com/title/tt13320622/mediaviewer/rm3056854785/?ref_=tt_ov_i"
+
+
+}
